9484381891059766ba9cb0d27130e435d9883af8
Chandel <2058920@cognizant.com>
2022-05-15 16:18:52 +0530

    Added notes how to run default spring security

--- SpringSecurityFull/src/main/java/com/learn/security/AppSecurityConfig.java
+++ SpringSecurityFull/src/main/java/com/learn/security/AppSecurityConfig.java
@@ -1,15 +1,24 @@
 package com.learn.security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter.UserDetailsServiceDelegator;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	
+	//the spring username is user and password  will be created by spring in console with base64
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -23,6 +32,10 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 			 and()
 			.httpBasic();
 	}
+	
+	
+	
+	
 	
 
 }
